9bdd5b77970f72bb992e0d8099f9e2c6bb129e4f
miko-github <mikoloism.github@gmail.com>
2021-01-22 23:22:38 +0330

    Add Prettier Setting

--- checkups.js
+++ checkups.js
@@ -1,8 +1,7 @@
 'use strict';
 //#region IS
 const is = {
-		empty: (subject) =>
-			typeof subject === `undefined` || subject === undefined,
+		empty: (subject) => typeof subject === `undefined` || subject === undefined,
 		func: (subject) =>
 			typeof subject === `function` || subject instanceof Function,
 		obj: (subject) => typeof subject === `object`,
@@ -21,9 +20,7 @@ const is = {
 		none: (subject) =>
 			has.usable(subject) &&
 			!has._null(subject) &&
-			(is.str(subject) || is.array(subject)
-				? subject.length > 0
-				: true) &&
+			(is.str(subject) || is.array(subject) ? subject.length > 0 : true) &&
 			subject !== '' &&
 			subject !== '' &&
 			subject !== `` &&
@@ -46,14 +43,17 @@ const is = {
 		txt: (subject) => /^[a-z]{1,}$/i.test(subject),
 		str: (subject) => /^[a-z#\$\^!~%,\.@\+?><]{1,}$/i.test(subject),
 		hex: (subject) => /^(#[a-f0-9]{3,12}|[a-f0-9]{3,12})$/i.test(subject),
-		num: (subject) => (/^[0-9]{1,}$/.test(subject)),
-		sign: (subject) => (/^-{0,1}[0-9]{1,}$/.test(subject)),
-		_sign: (subject) => (/^-{1}[0-9]{1,}$/.test(subject)),
-		float: (subject) => (/^[0-9]{1,}\.[0-9]{1,}$/.test(subject)),
-		signF: (subject) => (/^-{0,1}[0-9]{1,}\.[0-9]{1,}$/.test(subject)),
-		_signF: (subject) => (/^-{1}[0-9]{1,}\.[0-9]{1,}$/.test(subject)),
-		numF: (subject) => (/^[0-9]{1,}(\.[0-9]{1,}|[0-9]{0,})$/.test(subject)),
-		email: (subject) => (/^[a-z]{1,}([_a-z0-9\.]{0,}|[a-z])[a-z0-9]{1,}@{1}[a-z]{3,}(\.[a-z]{2,4}|\.[a-z]{2,4}\.[a-z]{2,4})$/i.test(subject))
+		num: (subject) => /^[0-9]{1,}$/.test(subject),
+		sign: (subject) => /^-{0,1}[0-9]{1,}$/.test(subject),
+		_sign: (subject) => /^-{1}[0-9]{1,}$/.test(subject),
+		float: (subject) => /^[0-9]{1,}\.[0-9]{1,}$/.test(subject),
+		signF: (subject) => /^-{0,1}[0-9]{1,}\.[0-9]{1,}$/.test(subject),
+		_signF: (subject) => /^-{1}[0-9]{1,}\.[0-9]{1,}$/.test(subject),
+		numF: (subject) => /^[0-9]{1,}(\.[0-9]{1,}|[0-9]{0,})$/.test(subject),
+		email: (subject) =>
+			/^[a-z]{1,}([_a-z0-9\.]{0,}|[a-z])[a-z0-9]{1,}@{1}[a-z]{3,}(\.[a-z]{2,4}|\.[a-z]{2,4}\.[a-z]{2,4})$/i.test(
+				subject
+			),
 	},
 	//#endregion REGEX - REGEXP
 	//#region DOCK - DOCUMENT
@@ -94,8 +94,7 @@ const is = {
 			else return false;
 
 			if (has.str(className)) {
-				if (has.array(c))
-					c.forEach((i) => i.classList.remove(className));
+				if (has.array(c)) c.forEach((i) => i.classList.remove(className));
 				else c.classList.remove(className);
 				return true;
 			} else return false;
@@ -130,8 +129,7 @@ const is = {
 					}
 					return true;
 				} else {
-					if (has.array(c))
-						c.forEach((i) => i.classList.toggle(className1));
+					if (has.array(c)) c.forEach((i) => i.classList.toggle(className1));
 					else c.classList.add(className1);
 					return true;
 				}
@@ -150,7 +148,7 @@ const is = {
 	run = {
 		error: (_false, _true) => {
 			console.error(
-				`[checkups.js]\n\nDATA_TYPE_ERROR : check data type and change it!\n\n>>> CURRENT_TYPE: "${typeof _false}"\n>>> SHOULD_TYPE_BE: "${_true}"\n\n`,
+				`[checkups.js]\n\nDATA_TYPE_ERROR : check data type and change it!\n\n>>> CURRENT_TYPE: "${typeof _false}"\n>>> SHOULD_TYPE_BE: "${_true}"\n\n`
 			);
 			return false; // this maybe return `undefined`;
 		},
@@ -162,29 +160,19 @@ const is = {
 		str: (subject, success, error = run.error) =>
 			has.str(subject) ? run.success(success) : error(subject, 'string'),
 		txt: (subject, success, error = run.error) =>
-			regex.txt(subject)
-				? run.success(success)
-				: error(subject, 'string'),
+			regex.txt(subject) ? run.success(success) : error(subject, 'string'),
 		obj: (subject, success, error = run.error) =>
 			has.obj(subject) ? run.success(success) : error(subject, 'object'),
 		array: (subject, success, error = run.error) =>
 			has.array(subject) ? run.success(success) : error(subject, 'array'),
 		num: (subject, success, error = run.error) =>
-			regex.num(subject)
-				? run.success(success)
-				: error(subject, 'number'),
+			regex.num(subject) ? run.success(success) : error(subject, 'number'),
 		sign: (subject, success, error = run.error) =>
-			regex.sign(subject)
-				? run.success(success)
-				: error(subject, 'number'),
+			regex.sign(subject) ? run.success(success) : error(subject, 'number'),
 		float: (subject, success, error = run.error) =>
-			regex.float(subject)
-				? run.success(success)
-				: error(subject, 'number'),
+			regex.float(subject) ? run.success(success) : error(subject, 'number'),
 		signF: (subject, success, error = run.error) =>
-			regex.signF(subject)
-				? run.success(success)
-				: error(subject, 'number'),
+			regex.signF(subject) ? run.success(success) : error(subject, 'number'),
 		none: (subject, success, error = run.error) =>
 			has.none(subject) ? run.success(success) : run.success(error),
 	},
